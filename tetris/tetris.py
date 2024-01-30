4373129e040c2e498b85308b949b80554f4c47e8
Zoha Peterson <zohapeterson@gmail.com>
2024-01-30 16:42:46 -0500

    this game is really hard to make

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -8,6 +8,7 @@ game_bg = "#000000"
 block_size = block_speed = game_width / 10
 score = 0
 tetriminos = []
+backgroundBlocks = []
 tetriminos_type = ["I", "J", "S", "Z", "L", "O", "T"]
 game_speed = 500
 
@@ -16,7 +17,7 @@ game_speed = 500
 def drawBackgroundGrid():
     for i in range(0, game_width, int(block_size)):
         for j in range(0, game_height, int(block_size)):
-            BackgroundBlock(i, j)
+            backgroundBlocks.append(BackgroundBlock(i, j))
 
 def randomTetriminos():
     return choice(tetriminos_type)
@@ -25,13 +26,21 @@ def randomOrientation():
     return randint(0, 3)
 
 def main():
+    activeTetrimino = tetriminos[len(tetriminos) - 1]
+    if(activeTetrimino.checkBottom()):
+        tetriminos.append(Tetrimino(block_size, block_size, "L", 0))
+        activeTetrimino = tetriminos[len(tetriminos) - 1]
+
     canvas.bind("<Up>", activeTetrimino.rotate)
+    canvas.bind("<Left>", activeTetrimino.set_leftMove)
+    canvas.bind("<Right>", activeTetrimino.set_rightMove)
     canvas.focus_set()
 
     activeTetrimino.ypos += block_speed
 
-    for i in range(4):
+    for i in range(len(activeTetrimino.squares)):
         canvas.move(activeTetrimino.squares[i].block_body, 0, block_speed)
+        activeTetrimino.points[i][1] += block_speed
 
     canvas.after(game_speed, main)
 
@@ -40,7 +49,9 @@ class Tetrimino:
     def __init__(self, xpos, ypos, type, orientation):
         self.xpos = xpos
         self.ypos = ypos
+        self.xmove = 0
         self.type = type
+        self.active = 1
         self.orientation = orientation
         self.move = 0
         self.squares = []
@@ -192,13 +203,13 @@ class Tetrimino:
         self.points.append([x1, y1])
         self.points.append([x2, y2])
         self.points.append([x3, y3])
-
+        
         if(self.move == 0):
             self.move = 1
-            for i in range(4):
+            for i in range(len(self.points)):
                 self.squares.append(Block(self.points[i][0], self.points[i][1], self.color))
         else:
-            for i in range(4):
+            for i in range(len(self.points)):
                 self.squares[i].moveBlock(self.points[i][0], self.points[i][1])
     
     def rotate(self, event):
@@ -211,6 +222,66 @@ class Tetrimino:
             self.orientation = 0
 
         self.createBody()
+    
+    def set_rightMove(self, event):
+        if(self.checkSideways("right")):
+            self.xmove = block_speed
+            self.xpos += self.xmove
+            for i in range(0, len(self.points)):
+                self.points[i][0] += self.xmove
+
+            self.move_sideways()
+
+    def set_leftMove(self, event):
+        if(self.checkSideways("left")):
+            self.xmove = -block_speed
+            self.xpos += self.xmove
+            for i in range(0, len(self.points)):
+                self.points[i][0] += self.xmove
+
+            self.move_sideways()
+    
+    def move_sideways(self):
+        for i in range(len(self.points)):
+            canvas.move(self.squares[i].block_body, self.xmove, 0)
+
+    def checkSideways(self, dir):
+        if(dir == "right"):
+            for i in range(0, len(self.points)):
+                if((self.points[i][0] + block_size) >= game_width):
+                    return False
+                for k in range(0, (len(tetriminos) - 1)):
+                    for j in range(0, (len(tetriminos[k].points))):
+                        if((self.points[i][0] + block_size) == tetriminos[k].points[j][0]):
+                            if((self.points[i][1]) == tetriminos[k].points[j][1]):   
+                                return False
+            return True
+        
+        elif(dir == "left"):
+            for i in range(0, len(self.points)):
+                if((self.points[i][0]) <= 0):
+                    return False
+                for k in range(0, (len(tetriminos) - 1)):
+                    for j in range(0, (len(tetriminos[k].points))):
+                        if((self.points[i][0]) == (tetriminos[k].points[j][0] + block_size)):
+                            if((self.points[i][1]) == tetriminos[k].points[j][1]):   
+                                return False
+            return True
+    
+    def checkBottom(self):
+        for i in range(0, len(self.points)):
+            if((self.points[i][1] + block_size) >= game_height):
+                self.active = 0
+                return True
+        
+            if(len(tetriminos) > 1):
+                for k in range(0, (len(tetriminos) - 1)):
+                    for j in range(0, (len(tetriminos[k].points))):
+                        if((self.points[i][1] + block_size) == tetriminos[k].points[j][1]):
+                            if(self.points[i][0] == tetriminos[k].points[j][0]):
+                                self.active = 0
+                                return True
+        return False
 class Block:
     def __init__(self, xpos, ypos, color):
         self.xpos = xpos
@@ -239,7 +310,7 @@ canvas.pack()
 drawBackgroundGrid()
 
 ## Run game
-activeTetrimino = Tetrimino(3 * block_size, 3 * block_size, "L", 0)
+tetriminos.append(Tetrimino(3*block_size, block_size, "L", 0))
 main()
 
 # Starts loop and event listener
