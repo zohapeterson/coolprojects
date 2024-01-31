3dea3b637b598a111a6433d2a48a16c1dc7b8091
Zoha Peterson <zohapeterson@gmail.com>
2024-01-31 12:45:20 -0500

    idk if i chose a terrible approach, but this was pretty difficult. it might need tweaking later

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -6,11 +6,11 @@ window = Tk()
 game_height, game_width = 500, 250
 game_bg = "#000000"
 block_size = block_speed = game_width / 10
-score = 0
 tetriminos = []
 backgroundBlocks = []
 tetriminos_type = ["I", "J", "S", "Z", "L", "O", "T"]
-game_speed = 500
+game_speed = 300
+score = 0
 
 ## Relevant Functions
 # Draw the background grid
@@ -25,38 +25,63 @@ def randomTetriminos():
 def randomOrientation():
     return randint(0, 3)
 
+def randomSpawn():
+    return randint(2, 8) * block_size
+
 def checkBreak():
     row_num = 0
+    remove_count = 0
     occupied = [0] * 20
     for a in range(0, game_height, int(block_size)):
         for b in range(0, game_width, int(block_size)):    
             for i in range(0, len(tetriminos)):
                 for j in range(0, len(tetriminos[i].points)):
                     if(tetriminos[i].points[j][0] == b and tetriminos[i].points[j][1] == a and tetriminos[i].active == 0):
-                        # print("Hello")
-                        # print(a)
                         occupied[row_num] += 1
         row_num += 1
     
-    for i in range(len(occupied)):
+    for c in range(0, len(occupied)):
+        if(occupied[c] == 10):
+            remove_count += 1
+
+    for i in range(0, len(occupied)):
         if(occupied[i] == 10):
             for j in range(0, int(game_width), int(block_size)):
-                # print("row: " + str(i * 25) + ", col: " + str(j))
-                for k in range(len(tetriminos)):
-                    for l in range(len(tetriminos[k].points)):
+                for k in range(0, len(tetriminos)):
+                    for l in range(0, len(tetriminos[k].points)):
                         if(tetriminos[k].points[l][0] == j and tetriminos[k].points[l][1] == (i*25)):
-                            # print("Remove. Row: " + str(i*25) + ", Col: " + str(j))
-                            pass
-                    
-            # print("Row " + str(i * 25) + " needs to be cleared.")
+                            tetriminos[k].squares[l].remove = 1
+    
+    for k in range(0, len(tetriminos)):
+        for l in range(0, len(tetriminos[k].points)):
+            if(tetriminos[k].squares[l].remove == 1):
+                canvas.delete(tetriminos[k].squares[l].block_body)                
+    
+    remove_rows = [i for i, count in enumerate(occupied) if count == 10]
 
-    occupied = [0] * 20
+    for row in remove_rows:
+        for i in range(row, 0, -1):
+            for j in range(0, len(tetriminos)):
+                for k in range(0, len(tetriminos[j].points)):
+                    if tetriminos[j].points[k][1] == (i * int(block_size)):
+                        tetriminos[j].points[k][1] += int(block_size)
+                        canvas.move(tetriminos[j].squares[k].block_body, 0, int(block_size))
+
+    return remove_count
         
+def checkLoss():
+    for i in range(0, len(tetriminos)):
+        for j in range(0, len(tetriminos[i].points)):
+            if(tetriminos[i].points[j][1] <= 0 and tetriminos[i].active == 0):
+                return True
+    return False
+
 def main():
-    checkBreak()
+    global score
+    score += checkBreak()
     activeTetrimino = tetriminos[len(tetriminos) - 1]
     if(activeTetrimino.checkBottom()):
-        tetriminos.append(Tetrimino(block_size, block_size, randomTetriminos(), randomOrientation()))
+        tetriminos.append(Tetrimino(randomSpawn(), -3 * block_size, randomTetriminos(), randomOrientation()))
         activeTetrimino = tetriminos[len(tetriminos) - 1]
 
     canvas.bind("<Up>", activeTetrimino.rotate)
@@ -70,7 +95,11 @@ def main():
         canvas.move(activeTetrimino.squares[i].block_body, 0, block_speed)
         activeTetrimino.points[i][1] += block_speed
 
-    canvas.after(game_speed, main)
+    after_control = canvas.after(game_speed, main)
+    
+    if(checkLoss()):
+        canvas.after_cancel(after_control)
+        print("You lose! Score: " + str(score))
 
 ## Create objects
 class Tetrimino:
@@ -241,15 +270,16 @@ class Tetrimino:
                 self.squares[i].moveBlock(self.points[i][0], self.points[i][1])
     
     def rotate(self, event):
-        for i in range(0, len(self.points)):
-            self.points.pop()
+        if(self.checkSideways("right") and self.checkSideways("left")):
+            for i in range(0, len(self.points)):
+                self.points.pop()
 
-        if(self.orientation < 3):
-            self.orientation += 1
-        else:
-            self.orientation = 0
+            if(self.orientation < 3):
+                self.orientation += 1
+            else:
+                self.orientation = 0
 
-        self.createBody()
+            self.createBody()
     
     def set_rightMove(self, event):
         if(self.checkSideways("right")):
@@ -315,6 +345,7 @@ class Block:
         self.xpos = xpos
         self.ypos = ypos
         self.color = color
+        self.remove = 0
         self.block_body = canvas.create_rectangle(self.xpos, self.ypos, self.xpos + block_size, self.ypos + block_size, fill=self.color, outline="white", width=1)  
 
     def moveBlock(self, newXpos, newYpos):
@@ -337,8 +368,7 @@ canvas.pack()
 drawBackgroundGrid()
 
 ## Run game
-# tetriminos.append(Tetrimino(3*block_size, block_size, "Z", 0))
-tetriminos.append(Tetrimino(3*block_size, block_size, randomTetriminos(), randomOrientation()))
+tetriminos.append(Tetrimino(randomSpawn(), -3 * block_size, randomTetriminos(), randomOrientation()))
 main()
 
 # Starts loop and event listener
